73462aa2e429096ce8674ddad02907a52cab94fa
grinat <paladin2012gnu@gmail.com>
2018-09-07 22:22:55 +0300

    ci: fix containers names

--- scripts/push-from-trevis.sh
+++ scripts/push-from-trevis.sh
@@ -5,8 +5,8 @@ docker ps -a
 docker login --username $DOCKER_U --password $DOCKER_P
 
 # Push builded images
-docker tag go-vue-blog-prod_go-vue-blog.api $DOCKER_U/go-vue-blog-api
-docker tag go-vue-blog-prod_go-vue-blog.node $DOCKER_U/go-vue-blog-node
+docker tag govueblogprod_go-vue-blog.api $DOCKER_U/go-vue-blog-api
+docker tag govueblogprod_go-vue-blog.node $DOCKER_U/go-vue-blog-node
 
 docker push $DOCKER_U/go-vue-blog-api
 docker push $DOCKER_U/go-vue-blog-node
--- scripts/test-and-build-on-travis.sh
+++ scripts/test-and-build-on-travis.sh
@@ -5,4 +5,4 @@ chmod 0777 frontend
 cd docker/prod
 
 # Build containers and Run tests
-docker-compose -f docker-compose.base.yml -f docker-compose.build.yml -f docker-compose.testing.yml up --abort-on-container-exit --exit-code-from go-vue-blog.node.testing
+docker-compose -f docker-compose.base.yml -f docker-compose.build.yml -f docker-compose.testing.yml up --abort-on-container-exit --exit-code-from go-vue-blog.node.testing || exit 1
